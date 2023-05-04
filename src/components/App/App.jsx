3a98ecd1d051334086579b2632b1a09184a6f88c
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-04 04:47:51 +0300

    done task 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,16 +7,28 @@ import ContactForm from '../ContactForm';
 import ContactsFilter from '../ContactsFilter';
 import ContactList from '../ContactList';
 
+const LS_KEY = 'contact_items';
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contactItems = JSON.parse(localStorage.getItem(LS_KEY));
+    if (contactItems) {
+      this.setState({ contacts: contactItems });
+    }
+    console.log(contactItems);
+    console.log(this.state.contacts);
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = newContact => {
     const { contacts } = this.state;
@@ -36,7 +48,7 @@ class App extends Component {
     // }
 
     this.setState(prevState => ({
-      contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+      contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
     }));
   };
 
